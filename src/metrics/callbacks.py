Fixed evaluation calculation framework

src/metrics/callbacks.py
@@ -1,40 +1,56 @@
 from __future__ import annotations
 
+from collections import defaultdict
 from typing import TYPE_CHECKING, Literal
 
 import numpy as np
-import pandas as pd
-import torch
-from mlflow.metrics import MetricValue
+from matplotlib import pyplot as plt
 from pytorch_lightning import Callback, LightningModule, Trainer
 from pytorch_lightning.loggers import MLFlowLogger
 from torch import Tensor
 
 from metrics.evaluation_metrics import (
+    PickStats,
     calculate_pick_differences,
-    compute_evaluation_metrics,
-    plot_histogram,
     calculate_precision_recall_f1,
+    get_f1_optimal_metrics,
+    plot_histogram,
     plot_precision_recall_f1,
-    calculate_roc_auc,
-    plot_roc_curve
+    plot_roc_curve,
 )
 
 if TYPE_CHECKING:
     from mlflow.tracking.client import MlflowClient
 
+SAMPLING_RATE = 100.0
+
+
+class CollectedStats:
+    stats: dict[str, list[PickStats]] = defaultdict(list)
+
+    def get_stats(self, phase: str) -> PickStats:
+        if phase not in self.stats:
+            raise ValueError(f"No stats collected for phase {phase}")
+        stats = self.stats[phase]
+        return PickStats(
+            predicted_samples=np.concatenate([s.predicted_samples for s in stats]),
+            labeled_samples=np.concatenate([s.labeled_samples for s in stats]),
+            predicted_certainty=np.concatenate([s.predicted_certainty for s in stats]),
+            noise_max=np.concatenate([s.noise_max for s in stats]),
+        )
+
+    def add(self, new_stats: dict[str, PickStats]) -> None:
+        for phase, stat in new_stats.items():
+            self.stats[phase].append(stat)
+
+    def clear(self) -> None:
+        self.stats.clear()
+
 
 class EvaluationMetrics(Callback):
     scores: list[float]
 
-    p_picks_labels: list[np.ndarray]
-    p_picks_predictions: list[np.ndarray]
-    s_picks_labels: list[np.ndarray]
-    s_pick_predictions: list[np.ndarray]
-    p_prob: list[np.ndarray]
-    s_prob: list[np.ndarray]
-    p_labels_binary: list[np.ndarray]
-    s_labels_binary: list[np.ndarray]
+    stats: CollectedStats
 
     mlflow_logger: MLFlowLogger
     experiment: MlflowClient
@@ -42,14 +58,7 @@ class EvaluationMetrics(Callback):
     def __init__(self, mlflow: MLFlowLogger) -> None:
         self.scores = []
 
-        self.p_picks_labels = []
-        self.p_picks_predictions = []
-        self.s_picks_labels = []
-        self.s_pick_predictions = []
-        self.p_prob = []
-        self.s_prob = []
-        self.p_labels_binary = []
-        self.s_labels_binary = []
+        self.stats = CollectedStats()
         self.mlflow_logger = mlflow
         self.experiment = mlflow.experiment
         super().__init__()
@@ -57,12 +66,6 @@ class EvaluationMetrics(Callback):
     def on_train_end(self, trainer: Trainer, pl_module: LightningModule) -> None:
         # Save the model
         ...
-
-    def get_picks(self, phase: str) -> tuple[list[np.ndarray], list[np.ndarray]]:
-        if phase == "P":
-            return self.p_picks_predictions, self.p_picks_labels, self.p_prob, self.p_labels_binary
-        else:
-            return self.s_pick_predictions, self.s_picks_labels, self.s_prob, self.s_labels_binary
 
     def on_validation_start(
         self,
@@ -78,21 +81,14 @@ class EvaluationMetrics(Callback):
         pl_module: LightningModule,
     ) -> None:
         for phase in ("P", "S"):
-            predictions, labels, probs, label_binary = self.get_picks(phase)
-            if not predictions or not labels:
+            pick_stats = self.stats.get_stats(phase)
+            if not pick_stats.n_samples:
                 print(f"No {phase} picks to log.")
                 continue
 
-            stats = compute_evaluation_metrics(
-                predicted=np.concatenate(predictions),
-                label=np.concatenate(labels),
-                sampling_rate=100.0,
-            )
-
             figure_hist = plot_histogram(
-                predicted=np.concatenate(predictions).ravel(),
-                label=np.concatenate(labels).ravel(),
-                stats=stats,
+                stats=pick_stats,
+                sampling_rate=SAMPLING_RATE,
                 title=f"{phase}-Pick Differences - Epoch {trainer.current_epoch}",
             )
 
@@ -103,73 +99,69 @@ class EvaluationMetrics(Callback):
             )
             print("Logged histogram for phase", phase)
 
-            metric_results = calculate_precision_recall_f1(offset= stats.offsets, final_prob=np.concatenate(probs).ravel())
-            figure_precision_recall_f1 = plot_precision_recall_f1(metric_results,
-                                     title=f"{phase}-Precision, Recall and F1 Score - Epoch {trainer.current_epoch}")
+            metric_results = calculate_precision_recall_f1(stats=pick_stats)
+
+            figure_precision_recall_f1 = plot_precision_recall_f1(
+                metric_results,
+                title=f"{phase}-Precision, Recall and F1 Score "
+                f"- Epoch {trainer.current_epoch}",
+            )
             self.experiment.log_figure(
                 self.mlflow_logger.run_id,
                 figure_precision_recall_f1,
-                f"precision_recall_f1_plots/{phase}-phase/epoch-{trainer.current_epoch:03d}.png",
+                f"precision_recall_f1_plots/"
+                f"{phase}-phase/epoch-{trainer.current_epoch:03d}.png",
             )
 
             print("Logged Metrics for phase", phase)
 
-            tpr, fpr, auc = calculate_roc_auc(offset= stats.offsets, final_prob=np.concatenate(probs).ravel(), label_binary=np.concatenate(label_binary).ravel())
-            figure_roc = plot_roc_curve(tpr, fpr, auc,  title=f"ROC Curve for {phase}-wave Picks - Epoch {trainer.current_epoch}")
+            figure_roc = plot_roc_curve(
+                stats=pick_stats,
+                title=f"ROC Curve for {phase}-wave Picks - Epoch {trainer.current_epoch}",
+            )
             self.experiment.log_figure(
-                            self.mlflow_logger.run_id,
-                            figure_roc,
-                            f"roc_curve_plot/{phase}-phase/epoch-{trainer.current_epoch:03d}.png",
-                        )
+                self.mlflow_logger.run_id,
+                figure_roc,
+                f"roc_curve_plot/{phase}-phase/epoch-{trainer.current_epoch:03d}.png",
+            )
             print("Logged ROC Curve for phase", phase)
+            optimal_detection_metrics = get_f1_optimal_metrics(metric_results)
+
+            sr = SAMPLING_RATE
             self.mlflow_logger.log_metrics(
                 {
-                    f"{phase}_mean_difference": stats.mean_difference,
-                    f"{phase}_median_difference": stats.median_difference,
-                    f"{phase}_mean_abs_error": stats.mean_abs_error,
-                    f"{phase}_rms_error": stats.rms_error,
-                    f"{phase}_precision": metric_results[0.6]["precision"],
-                    f"{phase}_recall": metric_results[0.6]["recall"],
-                    f"{phase}_f1_score": metric_results[0.6]["f1"],
-                    f"{phase}_auc": auc
-
+                    f"{phase}_mean_difference": pick_stats.mean_difference / sr,
+                    f"{phase}_median_difference": pick_stats.median_difference / sr,
+                    f"{phase}_mean_abs_error": pick_stats.mean_abs_error / sr,
+                    f"{phase}_rms_error": pick_stats.rms_error / sr,
+                    f"{phase}_precision": optimal_detection_metrics.precision,
+                    f"{phase}_recall": optimal_detection_metrics.recall,
+                    f"{phase}_f1_score": optimal_detection_metrics.f1_score,
+                    f"{phase}_optimal_threshold": optimal_detection_metrics.threshold,
+                    f"{phase}_auc": pick_stats.auc,
                 },
                 step=trainer.global_step,
             )
 
-        self.p_picks_labels.clear()
-        self.p_picks_predictions.clear()
-        self.s_picks_labels.clear()
-        self.s_pick_predictions.clear()
-        self.p_prob.clear()
-        self.s_prob.clear()
-        self.p_labels_binary.clear()
-        self.s_labels_binary.clear()
+        self.stats.clear()
+        plt.close("all")
 
-
-        
     def on_validation_batch_end(
         self,
         trainer: Trainer,
         pl_module: LightningModule,
-        outputs: Tensor,
+        outputs: tuple[Tensor, Tensor],
         batch: dict[Literal["X", "y"], Tensor],
         batch_idx: int,
         dataloader_idx: int = 0,
     ) -> None:
-
-
-        waveform_data = batch["X"]
         label_data = batch["y"]
-        label_predicted: Tensor = pl_module(waveform_data)
-        # print(outputs.shape)
+        # waveform_data = batch["X"]
+        # label_predicted: Tensor = pl_module(waveform_data)
+        _, label_predicted = outputs
         # print(label_predicted.shape)
         # print(outputs)
         # print(label_predicted)
-
-        torch.save(waveform_data.cpu(), "data/example_waveform.pt")
-        torch.save(label_data.cpu(), "data/example_labels.pt")
-        torch.save(label_predicted.cpu(), "data/example_predictions.pt")
 
         # p_differences, s_differences = get_pick_differences(label_data, label_predicted)
 
@@ -182,20 +174,10 @@ class EvaluationMetrics(Callback):
         # torch.save(label_data, "example_labels.pt")
         # torch.save(waveform_data, "example_waveform.pt")
         # torch.save(label_predicted, "example_predictions.pt")
-        p_predicted, p_labels, s_predicted, s_labels, p_prob, s_prob, p_labels_binary, s_labels_binary = calculate_pick_differences(
-            label_predicted.cpu(), label_data.cpu(), window_width=500
+        stats = calculate_pick_differences(
+            label_predicted.cpu(),
+            label_data.cpu(),
+            window_width=200,
         )
 
-        self.p_picks_predictions.append(p_predicted)
-        self.p_picks_labels.append(p_labels)
-
-        self.s_pick_predictions.append(s_predicted)
-        self.s_picks_labels.append(s_labels)
-
-        self.p_prob.append(p_prob)
-        self.s_prob.append(s_prob)
-
-
-        self.p_labels_binary.append(p_labels_binary)
-        self.s_labels_binary.append(s_labels_binary)
-
+        self.stats.add(stats)

src/seisbench_training/main.py
@@ -10,10 +10,12 @@ import torch
 import typer
 from omegaconf import OmegaConf
 from pytorch_lightning.callbacks import ModelCheckpoint
-from pytorch_lightning.loggers import CSVLogger, MLFlowLogger
+from pytorch_lightning.loggers import MLFlowLogger
 from seisbench.util import worker_seeding
 from torch.utils.data import DataLoader
+
 from metrics.callbacks import EvaluationMetrics
+
 from .utils.model_utils import SeisBenchLit, phase_dict
 
 app = typer.Typer()
@@ -111,9 +113,14 @@ def train_seisbench(cfg):
     data = DatasetClass(
         component_order=dataset.component_orders,
         sampling_rate=dataset.sampling_rate,
+        cache="full",
+        # cache="trace",  # 'full' caches the full block
     )
     log.info("Dataset loaded successfully.")
     train, dev, test = data.train_dev_test()
+    train.preload_waveforms(pbar=True)
+    dev.preload_waveforms(pbar=True)
+    test.preload_waveforms(pbar=True)
 
     log.info("Setting up generators...")
     train_gen = sbg.GenericGenerator(train)
@@ -125,12 +132,13 @@ def train_seisbench(cfg):
         lr=cfg.training.lr,
         sigma=cfg.augmentations.prob_labeller_default.sigma,
         pretrained_model_name=cfg.training.pretrained_model_name,
-        transfer_learning =cfg.training.transfer_learning,
     )
-    if cfg.training.transfer_learning == True:
-            filename=f"best_model_{cfg.augmentations.prob_labeller_default.shape}_transfer_learning_{cfg.training.pretrained_model_name}"
-    else:
-            filename=f"best_model_{cfg.augmentations.prob_labeller_default.shape}_no_transfer_learning"
+    tpl_transfer = (
+        ""
+        if not cfg.training.pretrained_model_name
+        else f"_from_{cfg.training.pretrained_model_name}"
+    )
+    filename = f"best_model_sigma_{cfg.augmentations.prob_labeller_default.shape}{tpl_transfer}"
     log.info("Adding Augmentation...")
     train_gen.add_augmentations(augmentations)
     dev_gen.add_augmentations(augmentations)
@@ -160,9 +168,12 @@ def train_seisbench(cfg):
         num_workers=cfg.training.num_workers,
     )
 
-
-    mlf_logger = MLFlowLogger(experiment_name=cfg.experiment_name, log_model=True)
-
+    mlf_logger = MLFlowLogger(
+        experiment_name=cfg.experiment_name,
+        log_model=True,
+    )
+    # TODO: Add SystemMetrics callback
+    # https://github.com/Lightning-AI/pytorch-lightning/issues/20563
     checkpoint_callback = ModelCheckpoint(
         filename=filename,
         monitor="val_loss",
@@ -185,11 +196,10 @@ def train_seisbench(cfg):
         callbacks=callbacks,
         accelerator="gpu",
         log_every_n_steps=1,
-        devices=2,
-        strategy="ddp",
+        devices=1,
     )
 
-    trainer.fit(pl_model, dev_loader, test_loader)
+    trainer.fit(pl_model, train_loader, test_loader)
     mlf_logger.experiment.log_dict(
         run_id=mlf_logger.run_id,
         dictionary=OmegaConf.to_container(cfg, resolve=True),

src/seisbench_training/utils/model_utils.py
@@ -1,10 +1,7 @@
-import logging
-
 import numpy as np
 import pytorch_lightning as pl
 import seisbench.generate as sbg
 import seisbench.models as sbm
-import hydra
 import torch
 
 phase_dict = {
@@ -49,8 +46,7 @@ class SeisBenchLit(pl.LightningModule):
         self,
         lr,
         sigma,
-        transfer_learning: str = None,
-        pretrained_model_name: str = None,
+        pretrained_model_name: str = "",
         sample_boundaries=(None, None),
         optimizer_params=None,
         **kwargs,
@@ -62,9 +58,9 @@ class SeisBenchLit(pl.LightningModule):
         self.sample_boundaries = sample_boundaries
         self.optimizer_params = optimizer_params or {}
         self.loss = loss_fn
-        self.transfer_learning = transfer_learning
+
         self.pretrained_model_name = pretrained_model_name
-        if self.transfer_learning == True:
+        if self.pretrained_model_name:
             self.model = sbm.PhaseNet.from_pretrained(
                 self.pretrained_model_name, **kwargs
             )
@@ -75,20 +71,20 @@ class SeisBenchLit(pl.LightningModule):
         return self.model(x)
 
     def shared_step(self, batch):
-        x = batch["X"]
+        x = self.model.annotate_batch_pre(batch["X"], {})
         y_true = batch["y"]
         y_pred = self.model(x)
-        return self.loss(y_pred, y_true)
+        return self.loss(y_pred, y_true), y_pred
 
     def training_step(self, batch, batch_idx):
-        loss = self.shared_step(batch)
+        loss, _ = self.shared_step(batch)
         self.log("train_loss", loss, prog_bar=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        loss = self.shared_step(batch)
+        loss, y_pred = self.shared_step(batch)
         self.log("val_loss", loss, prog_bar=True)
-        return loss
+        return loss, y_pred
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), **self.optimizer_params)
@@ -118,7 +114,9 @@ class SeisBenchLit(pl.LightningModule):
             sbg.ChangeDtype(np.float32),
             sbg.Normalize(demean_axis=-1, amp_norm_axis=-1, amp_norm_type="peak"),
             sbg.ProbabilisticLabeller(
-                label_columns=phase_dict, sigma=self.sigma, dim=0,
+                label_columns=phase_dict,
+                sigma=self.sigma,
+                dim=0,
             ),
         ]
 